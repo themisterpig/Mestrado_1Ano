5aa7d9f2d98b08c16afe427453d7cabac8e2a31e
themisterpig <josedfl2001@gmail.com>
2022-12-08 22:44:48 +0000

    Create FuncionarCores.R

---
+++ Programação para Cientistas de Dados/Primeiro trabalho/FuncionarCores.R
@@ -0,0 +1,56 @@
+dftest <- data.frame(x = c(1,6,4,2,7,4,5,3), y = c(1,1,6,4,4,6,2,3))
+
+quickhull = function(df){
+  
+  convex_hull = data.frame(x = c(), y = c())
+  
+  sorted = df[order(df$x),]
+  
+  p1 = sorted[1,]
+  p2 = sorted[nrow(sorted),]
+  
+  convex_hull = rbind(convex_hull, p1,p2)
+  
+  sorted = sorted[-c(1,nrow(sorted)),]
+  
+  above = create_segment(p1,p2,sorted,1)
+  below = create_segment(p1,p2,sorted,-1)
+  
+  #plot sorted points
+  plot(dftest, type = "p", col = "blue", pch = 16, cex = 1)
+  #line between p1 and p2
+  lines(c(p1$x,p2$x), c(p1$y,p2$y), col = "red", lwd = 2)
+  #above points
+  points(above, col = "green", pch = 16, cex = 1.5)
+  #below points
+  points(below, col = "orange", pch = 16, cex = 1.5)
+}
+
+
+create_segment = function(p1,p2,df,side){
+  above = data.frame(x = c(), y = c())
+  below = data.frame(x = c(), y = c())
+  
+  if(p2$x - p1$x == 0){
+  return (data.frame(x = c(), y = c()))
+  }
+  m = (p2$y - p1$y)/(p2$x - p1$x)
+  c = p1$y - m*p1$x
+  
+  for (i in 1:nrow(df)){
+    if (df[i,2] > m*df[i,1] + c)
+      above = rbind(above, df[i,])
+    else
+      below = rbind(below, df[i,])
+  }
+  
+  if(side == 1){
+    return(above)
+  }else{
+    return(below)
+  }
+  
+}
+
+quickhull(dftest)
+
